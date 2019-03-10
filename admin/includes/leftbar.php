27dc28936bf631b163fae40e0d2ab0c1a48fad54
hth_developers <talhamanzoor006@gmail.com>
2019-03-10 23:08:49 +0500

    updated

--- admin/includes/leftbar.php
+++ admin/includes/leftbar.php
@@ -12,7 +12,7 @@
 </li>
 
 
-				<li><a href="add-donor.php"><i class="fa fa-edit"></i> Add User</a></li>
+				<li><a href="add-donor.php"><i class="fa fa-edit"></i> Add Products</a></li>
 				<li><a href="donor-list.php"><i class="fa fa-users"></i> Donor User List</a></li>
 
 				<li><a href="manage-conactusquery.php"><i class="fa fa-desktop""></i> Manage Conatctus Query</a></li>
